a4da252aaaca8576d8918221ea1923760babcb79
Pravoslav <pravoslav.zilka@gmail.com>
2021-03-31 22:36:39 +0200

    login page - frontend only

--- blueprints/user/__init__.py
+++ blueprints/user/__init__.py
@@ -1,12 +1,8 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, render_template
 
-user = Blueprint('simple_page', __name__,template_folder='templates')
+user = Blueprint('user', __name__,template_folder='templates')
 
 
-@user.route('/<page>')
-def show(page):
-    try:
-        return render_template('pages/%s.html' % page)
-    except TemplateNotFound:
-        abort(404)
+@user.route('/hello')
+def login_page():
+    return render_template("user/login_page.html")
